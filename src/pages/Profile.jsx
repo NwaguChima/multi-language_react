5d8b14ca7a0b4a6f266af1d1c5541b750100d51c
NwaguChima <nwaguchima48@gmail.com>
2022-09-19 08:11:35 +0100

    added input fields

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="container mt-5 col-9 col-md-6">
+      <h1 className="text-center">Profile</h1>
+
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="John Doe"
+          id="name"
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="age">Age:</label>
+        <input
+          type="number"
+          className="form-control"
+          placeholder="25"
+          id="age"
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="john@mail.com"
+          id="email"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
