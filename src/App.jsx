b1f95986941752102819d450564543f072341a6c
NwaguChima <nwaguchima48@gmail.com>
2022-09-19 07:45:18 +0100

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// components import
+import Header from "./components/nav/Header";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Lets go react lingual</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
