4472e4c4a8ea40c8a7600ede669e8c7d6790aca6
NwaguChima <nwaguchima48@gmail.com>
2022-09-19 08:48:53 +0100

    translated profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Profile = () => {
+  const { t } = useTranslation(["profile", "common"]);
+  console.log(t());
+
   return (
     <div className="container mt-5 col-9 col-md-6">
-      <h1 className="text-center">Profile</h1>
+      <h1 className="text-center">{t("common:profile")}</h1>
 
       <div className="form-group">
-        <label htmlFor="name">Name:</label>
+        <label htmlFor="name">{t("profile:name")}:</label>
         <input
           type="text"
           className="form-control"
@@ -16,7 +20,7 @@ const Profile = () => {
       </div>
 
       <div className="form-group">
-        <label htmlFor="age">Age:</label>
+        <label htmlFor="age">{t("profile:age")}:</label>
         <input
           type="number"
           className="form-control"
@@ -26,7 +30,7 @@ const Profile = () => {
       </div>
 
       <div className="form-group">
-        <label htmlFor="email">Email:</label>
+        <label htmlFor="email">{t("profile:email")}:</label>
         <input
           type="text"
           className="form-control"
@@ -36,7 +40,7 @@ const Profile = () => {
       </div>
       <br />
       <div className="text-center">
-        <button className="btn btn-dark">Submit</button>
+        <button className="btn btn-dark">{t("profile:submit")}</button>
       </div>
     </div>
   );
