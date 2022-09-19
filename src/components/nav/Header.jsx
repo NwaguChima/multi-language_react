d81105b09b430d3bda889bd62459f7671d3804a0
NwaguChima <nwaguchima48@gmail.com>
2022-09-19 08:08:24 +0100

    completed header

--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
         Home
       </Link>
@@ -18,6 +18,22 @@ const Header = () => {
       >
         <span className="navbar-toggler-icon"></span>
       </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <select className="nav-link bg-dark border-0 ml-1 mr-2">
+              <option value="en">English</option>
+              <option value="fr">Français</option>
+              <option value="es">Español</option>
+            </select>
+          </li>
+          <li className="nav-item ml-2">
+            <Link className="nav-link" to="/profile">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
